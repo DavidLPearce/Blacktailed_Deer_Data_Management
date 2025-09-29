37b916ce0cd2d6c7d9bcb291358abe6e36303cae
DavidLPearce <davepearce5819@gmail.com>
2025-09-29 14:07:52 -0700

    Ran through data from just one WMU in 2024 - not too bad but suspect earlier years may be tricky.

---
+++ Scripts/2024_Dixon.R
@@ -0,0 +1,134 @@
+# Author: David L. Pearce
+# Description:
+#       Data wrangling for Columbia black-tailed deer in the Dixon WMU in 2024
+#              
+#              
+#              
+#                           
+
+# ------------------------------------------------------------------------------
+#
+#                               Load Packages
+#
+# ------------------------------------------------------------------------------
+
+# Load packages
+library(tidyverse)
+library(readxl)
+
+# Load Functions
+source("./Scripts/Functions/AlleleID_Suffix_Function.R")
+
+# Set seed, scientific notation, and workplace
+set.seed(123)
+options(scipen = 9999)
+setwd(".")
+
+# ------------------------------------------------------------------------------
+#
+#                                 Load Data
+#
+# ------------------------------------------------------------------------------
+
+# Path to Excel file
+path <- "./Data/Raw/2024-Dixon_BTD_14July25.xlsx"
+
+# Each sheet in Excel File
+sheets <- excel_sheets(path)
+
+# Read all sheets into a list of data frames
+df_list <- lapply(sheets, function(x) readxl::read_excel(path, sheet = x))
+
+# Name each df according to sheet name
+names(df_list) <- sheets
+
+# Take a look
+print(df_list)
+
+
+# ------------------------------------------------------------------------------
+#
+#                               Data Wrangling
+#
+# ------------------------------------------------------------------------------
+
+# -----------------------
+# Separating
+# -----------------------
+
+# ---- Note ----
+# The genetic data already has Lat and Long, don't need to add that in, 
+# and there is no other information that is needed `2024 Dixon Dog Samples` 
+
+# Extract Genetic, Geosepatial, and Assignment into individual df
+data_gen <- df_list$`2024 All Dixon Genotypes`
+data_assn <- df_list$`2024 DxD Deer Assignment`
+
+# Inspect each df
+View(data_gen)
+View(data_assn)
+
+# -----------------------
+# Cleaning
+# -----------------------
+
+# Columns were not named correctly due to notes above each sheet, column names
+# are actually row 1
+colnames(data_gen) <- as.character(unlist(data_gen[1, ])) # row 1 as column name
+data_gen <- data_gen[-1, ]# Drop the first row
+print(data_gen)
+
+# The genetic data has identical column names for each allele call. Adding a 
+# .1 and .2 to the allele to fix this
+names(data_gen) <- fix_alleles(names(data_gen))
+print(data_gen)
+
+# data_assn is not - repeat
+colnames(data_assn) <- as.character(unlist(data_assn[1, ])) # row 1 as column name
+data_assn <- data_assn[-1, ]
+print(data_assn)
+
+# -----------------------
+# Merging Together
+# -----------------------
+
+# Merging Deer Assignment Number from data_assn to data_gen but nothing else 
+data_merge <- data_gen %>%
+                      left_join(
+                        data_assn %>% select(`ODFW Sample #`, `Deer Assignment Number`),
+                        by = c("ODFW ID" = "ODFW Sample #")
+                      )
+
+# Take a look
+View(data_merge)
+
+# -----------------------
+# Reorganizing and Renaming
+# -----------------------
+
+# Add in a column for WMU for later on when all years/WMUs are compiled together
+data_merge$WMU <- "Dixon"
+
+# Add in a year column
+data_merge$Year <- 2024
+
+# Renaming column names for consistency across years. 
+names(data_merge) <- gsub(" ", "_", names(data_merge)) # spaces to underscores
+data_merge <- data_merge %>% # Manual changes
+  rename(
+    "Fawn" = "Fawn?",
+    "Nloci" = "#_of_loci", 
+    "Sex" = "sex",
+    "DAN" = "Deer_Assignment_Number"
+)
+print(names(data_merge)) # Take a look
+
+# -----------------------
+# Exporting
+# -----------------------
+
+saveRDS(data_merge, file = "./Data/Cleaned/rds/2024Dixon.rds")
+write.csv(data_merge, file = "./Data/Cleaned/csv/2024Dixon.csv")
+
+
+# ----------------------------- End of Script -----------------------------
