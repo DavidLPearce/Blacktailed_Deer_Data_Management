73bfe9f2de4d11879abe04395be48894aa588b2f
DavidLPearce <davepearce5819@gmail.com>
2025-10-10 14:50:48 -0700

    Removed dataframe - will copy in final from DatabaseFormat.R after I have gone through the files and pulled everything.

--- Scripts/YearWMU_processing/Compile_YearsWMUs.R
+++ Scripts/YearWMU_processing/Compile_YearsWMUs.R
@@ -1,6 +1,6 @@
 # Author: David L. Pearce
 # Description:
-#       Compiling data from 2012-2024 for Columbia black-tailed deer 
+#       Compiling data from 2012-2024 for Columbian black-tailed deer 
 #        microsatellite genetic data
 #              
 #              
@@ -47,57 +47,7 @@ files <- list.files(directory, pattern = "\\.rds$", full.names = TRUE)
 # ------------------------------------------------------------------------------
 
 # Create a blank data frame with all of the desired columns 
-cbtd_data <- data.frame(
-                  ODFW_ID = character(), 
-                  OSU_ID = character(), 
-                  Year = numeric(), 
-                  WMU = character(),
-                  MgmtArea = character(), 
-                  Collection_method = character(),
-                  Latitude = numeric(), 
-                  Longitude = numeric(),
-                  Species = character(),  
-                  Sex = character(), 
-                  DAN = character(),
-                  standardized_markers = character(),
-                  Nmarkers = numeric(),
-                  C273.1 = numeric(), 
-                  C273.2 = numeric(),  
-                  C89.1 = numeric(), 
-                  C89.2 = numeric(),
-                  OdhE.1 = numeric(), 
-                  OdhE.2 = numeric(), 
-                  SBTD05.1 = numeric(), 
-                  SBTD05.2 = numeric(), 
-                  SBTD06.1 = numeric(), 
-                  SBTD06.2 = numeric(),
-                  T159s.1 = numeric(), 
-                  T159s.2 = numeric(),
-                  T7.1 = numeric(), 
-                  T7.2 = numeric(), 
-                  markers_2012 = character(),
-                  SBTD04.1 = numeric(),
-                  SBTD04.2 = numeric(), 
-                  SBTD07.1 = numeric(),
-                  SBTD07.2 = numeric(),
-                  markers_2016 = character(),
-                  B.1 = numeric(), 
-                  B.2 = numeric(),
-                  C.1 = numeric(), 
-                  C.2 = numeric(),
-                  H.1 = numeric(), 
-                  H.2 = numeric(),
-                  N.1 = numeric(), 
-                  N.2 = numeric(),
-                  R.1 = numeric(), 
-                  R.2 = numeric(),
-                  V.1 = numeric(), 
-                  V.2 = numeric(),
-                  Marker_notes = character(), 
-                  Geo_notes = character(),
-                  Other_notes = character(),
-                  stringsAsFactors = FALSE  # Ensure character columns are treated as strings
-)
+
 
 # Take a look
 str(cbtd_data)
